cd522334f47aaa7e5b00d432502ca9db899cde91
Mateusz Gorczycki <mateusz.gorczycki@whiteaster.com>
2020-08-11 13:59:59 +0200

    Add base of project

---
+++ apps/angular-clean-project-template/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'angular-clean-project-template-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'angular-clean-project-template';
+}
